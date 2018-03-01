08934c05c47faeb4241146ef654469b16476a42f
Alexandre Magno Monteiro <alexandre.magno@forlogic.net>
2018-03-01 14:54:38 -0300

    models

--- src/app/users/shared/user.model.ts
+++ src/app/users/shared/user.model.ts
@@ -2,6 +2,10 @@
 export class User {
   username: string;
   email: string;
+  birthDate: Date;
+  workLoad: number;
+  schooling?: string;
   firstName?: string;
   lastName?: string;
+  phone?: string;
 }
